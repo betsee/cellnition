ccdfbd35af61197fdfb3aeac38955f2237801222
pietakio <alexis.pietak@gmail.com>
2025-03-23 13:30:52 -0400

    Cellnition 0.2 started.

--- cellnition/__init__.py
+++ cellnition/__init__.py
@@ -56,7 +56,7 @@ filterwarnings(
 # ....................{ GLOBALS                            }....................
 # Declare PEP 8-compliant version constants expected by external automation.
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 '''
 Human-readable application version as a ``.``-delimited string.
 
